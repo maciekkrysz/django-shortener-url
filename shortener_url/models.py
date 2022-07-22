080ca79130164d2aa953b776118deeb2f8b7bc31
Maciej Krysztofiak <maciekkrysz1@gmail.com>
2022-07-23 00:39:47 +0200

    add link model with manager and queryset

--- shortener_url/models.py
+++ shortener_url/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.db.models import Q
 
 # Create your models here.
 
 
 class Link(models.Model):
     alias = models.CharField(max_length=200)
-    link_to = models.CharField(max_length=2048)
+    link_to = models.URLField(max_length=2048)
+
+
+class LinkQuerySet(models.QuerySet):
+    def search(self, query=None):
+        if query is None or query == "":
+            return self.none()
+        lookups = Q(alias__icontains=query)
+        return self.filter(lookups)
+
+
+class LinkManager(models.Manager):
+    def get_queryset(self):
+        return LinkQuerySet(self.model, using=self._db)
+
+    def search(self, query=None):
+        return self.get_queryset().search(query=query)
