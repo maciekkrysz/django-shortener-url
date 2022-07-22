1024babf6cfe02d7942dc007b1cf2259da0895d5
Maciej Krysztofiak <maciekkrysz1@gmail.com>
2022-07-22 14:19:40 +0200

    add model Link

--- shortener_url/models.py
+++ shortener_url/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Link(models.Model):
+    alias = models.CharField(max_length=200)
+    link_to = models.CharField(max_length=2048)
